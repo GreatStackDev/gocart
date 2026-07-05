7cb6bc3a4d81966c32c1666e9e0355329cd09bcf
Molele Kishoez <molelekishoez@gmail.com>
2026-07-05 07:31:50 +0200

    feat: implement admin coupon management interface and API routes

--- app/api/admin/coupon/route.js
+++ app/api/admin/coupon/route.js
@@ -2,6 +2,7 @@ import { getAuth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 import authAdmin from "@/middlewares/authAdmin"
 import prisma from "@/lib/prisma"
+import { inngest } from "@/inngest/client"
 
 
 
